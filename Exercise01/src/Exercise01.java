38dcc4cd8ed482db87adf2922f4ef7fe1a4b2e7b
Jennifer Mary Tran <98693224+Tran0222@users.noreply.github.com>
2022-01-30 07:51:08 -0500

    Add files via upload

---
+++ Exercise01/src/Exercise01.java
@@ -0,0 +1,31 @@
+/* Name: Jennifer Tran
+ * Lab Professor: Mohammed Patoary 
+ * Due Date: September 24,2011
+ * Modified: September 16,2011
+ * Description: Lab 1: exercise 1
+*/
+
+/* Description of program: to make a castle using ASCII*/
+public class Exercise01 { //class purpose is show a castle with programmer name
+
+	public static void main (String [] args) { // Start of method main
+	
+	/* Output: print on lines 18 - 31 to build an ASCII castle. Line 31 include
+	 *  name */
+		
+	System.out.println("   >>>              >>>"); 
+	System.out.println("   |                |"); 
+	System.out.println(" = == =           = == ="); 
+	System.out.println(" |    |     >>>   |    |");
+	System.out.println(" | [] |     |     | [] |");
+	System.out.println(" |    |= = = = = =|    |");
+	System.out.println(" | [] | []     [] | [] |");
+	System.out.println(" |    |    ===    |    |");
+	System.out.println(" | [] |   |   |   | [] |");
+	System.out.println(" |    |   |   |   |    |");
+	System.out.println("~~~~~~~~~ /   /~~~~~~~~~");
+	System.out.println("~~~~~~~~~/   /~~~~~~~~~ ");
+	System.out.println("        /   /           ");
+	System.out.println("Program by Jennifer Tran"); // print out full name of programmer
+} // End of method main
+	} // End of class body
